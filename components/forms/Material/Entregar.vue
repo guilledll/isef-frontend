28f9fd7d77d3a025882727c55036931904ef6ec8
Joaquín <gomezjoaquin@protonmail.com>
2021-11-11 23:28:46 -0300

    entregar

---
+++ components/forms/Material/Entregar.vue
@@ -0,0 +1,151 @@
+<template>
+  <form @submit.prevent="entregarMaterial">
+    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+      <div class="sm:flex sm:items-start">
+        <ModalLeftIcon />
+        <div class="modal-form-body">
+          <h3 class="modal-form-heading">Entregar material</h3>
+          <p class="mb-3">Entregar material a un alumno.</p>
+          <div class="space-y-2">
+            <div class="mb-5 grid grid-cols-2 gap-3 text-left sm:gap-5">
+              <GlobalInputData
+                v-for="res in reserva"
+                :key="res.key"
+                :title="res.title"
+                :value="datoReserva(res.key)"
+                :color="res.color"
+                :svg="res.svg"
+              />
+            </div>
+            <div>
+              {{ reserva }}
+              <FormTextarea
+                id="nota"
+                v-model.trim="form.nota"
+                name="nota"
+                placeholder="Notas (opcional)"
+                :required="false"
+                :error="hasError($v.form.nota, 'nota')"
+                @input="fieldReset($v.form.nota, 'nota')"
+                @blur="touch($v.form.nota)"
+              >
+                <LazyFormError
+                  v-if="hasError($v.form.nota, 'nota')"
+                  :text="errorText($v.form.nota, 'nota')"
+                />
+              </FormTextarea>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <ModalFooter
+      text="Entregar material"
+      :disabled="disabled"
+      @close="closeModal"
+    />
+  </form>
+</template>
+
+<script>
+import FormValidationMixin from '@/mixins/FormValidationMixin';
+import { validationMixin } from 'vuelidate';
+import { required, integer, maxLength } from 'vuelidate/lib/validators';
+import { updatedDiff } from 'deep-object-diff';
+export default {
+  mixins: [validationMixin, FormValidationMixin],
+  props: {
+    isView: { type: Boolean, default: false },
+    id: { type: Number, default: null },
+  },
+  data() {
+    return {
+      form: {
+        id: '',
+        user: '',
+        user_ci: '',
+        deposito_id: '',
+        deposito: '',
+        categoria_id: '',
+        guardia_ci: this.$auth.user.ci,
+        nota: '',
+        material: '',
+      },
+    };
+  },
+  computed: {
+    reserva() {
+      return this.$store.state.reservas.reserva;
+    },
+    disabled() {
+      return Object.keys(updatedDiff(this.reserva, this.form)).length == 0;
+    },
+    materiales() {
+      return this.$store.state.reservas.materialesDisponibles;
+    },
+  },
+  mounted() {
+    this.$store.dispatch('reservas/get', this.id);
+    this.form.id = this.reserva.id;
+    this.form.user = this.reserva.user;
+    this.form.user_ci = this.reserva.user_ci;
+    this.form.guardia_ci = this.reserva.guardia_ci;
+    this.form.deposito = this.reserva.deposito;
+    this.form.inicio = this.reserva.inicio;
+    this.form.fin = this.reserva.fin;
+    this.form.estado = this.reserva.estado;
+    this.form.nota_usuario = this.reserva.nota_usuario;
+    this.form.material = this.reserva.material_id;
+  },
+  validations: {
+    form: {
+      id: {
+        required,
+        integer,
+      },
+      nombre: {
+        required,
+        maxLength: maxLength(50),
+      },
+      deposito_id: {
+        required,
+        integer,
+      },
+      categoria_id: {
+        required,
+        integer,
+      },
+      cantidad: {
+        required,
+        integer,
+      },
+      nota: {
+        maxLength: maxLength(255),
+      },
+    },
+  },
+  methods: {
+    selectReserva(value) {
+      this.form.reserva = value;
+    },
+    entregarReserva() {
+      console.log(this.reserva);
+      if (this.invalid()) return;
+      this.$store
+        .dispatch('reservas/updateEstado', this.form)
+        .then(() => {
+          if (this.isView) {
+            this.$emit('close');
+          } else {
+            this.$emit('actualizado');
+          }
+        })
+        .catch((e) => (this.errors = e.response.data.errors));
+    },
+    closeModal() {
+      if (!this.isView) this.$store.dispatch('reservas/clear');
+      this.$emit('close');
+    },
+  },
+};
+</script>
---
+++ pages/admin/Reservas.vue
@@ -0,0 +1,229 @@
+<template>
+  <div>
+    <GlobalHeader :title="pageHeader.title" :text="pageHeader.text" />
+    <!--Filtrar por deposito/estado-->
+    <div>
+      <FormSelect
+        id="deposito_id"
+        v-model.trim="filtro.id"
+        name="deposito_id"
+        @change="filtrar"
+      >
+        <template #options>
+          <option value="0">Seleccionar</option>
+          <option
+            v-for="(contenido, index) in contenidoFiltrado"
+            :key="index"
+            :value="contenido.id || contenido"
+          >
+            <span v-if="contenido.id"> {{ contenido.nombre }} </span>
+            <span v-else> {{ mostrarEstado(contenido) }}</span>
+          </option>
+        </template>
+      </FormSelect>
+      <label for="deposito">Deposito</label>
+      <input
+        id="deposito"
+        v-model="filtro.contenido"
+        name="filtro"
+        type="radio"
+        value="deposito_id"
+        @change="cambiarFiltro"
+      />
+      <label for="estado">Estado</label>
+      <input
+        id="estado"
+        v-model="filtro.contenido"
+        name="filtro"
+        type="radio"
+        value="estado"
+        @change="cambiarFiltro"
+      />
+    </div>
+    <div class="flex flex-col gap-3 lg:flex-row">
+      <Table>
+        <template #head>
+          <TableHead :header="table.header" />
+        </template>
+        <template #body>
+          <tr v-for="reserva in reservas" :key="reserva.id">
+            <td class="table-td text-gray-500">
+              <router-link
+                :to="`/reservas/${reserva.id}`"
+                class="text-black hover:text-blue-600 hover:underline"
+                @click.native="seleccionarReserva('view', reserva)"
+              >
+                {{ reserva.user_ci }}
+              </router-link>
+            </td>
+            <td class="table-td text-gray-500">
+              <router-link
+                :to="`/categorias/${reserva.categoria_id}`"
+                class="hover:text-blue-600 hover:underline"
+                @click.native="verCategoria(reserva.categoria_id)"
+              >
+                {{ reserva.guardia_ci }}
+              </router-link>
+            </td>
+            <td class="table-td text-gray-500">
+              <router-link
+                :to="`/depositos/${reserva.deposito_id}`"
+                class="hover:text-blue-600 hover:underline"
+                @click.native="verDeposito(reserva.deposito_id)"
+              >
+                {{ reserva.deposito }}
+              </router-link>
+            </td>
+            <td class="table-td text-gray-500">
+              {{ reserva.inicio }}
+            </td>
+            <td class="table-td text-gray-500">
+              {{ reserva.fin }}
+            </td>
+            <td class="table-td text-gray-500">
+              {{ mostrarEstado(reserva.estado) }}
+            </td>
+            <td class="table-td text-right">
+              <TableButton
+                type="view"
+                @click="$router.push(`/reservas/${reserva.id}`)"
+              />
+              <TableButton
+                type="delete"
+                @click="seleccionarReserva('del', reserva)"
+              />
+              <TableButton
+                type="edit"
+                @click="seleccionarReserva('mod', reserva)"
+              />
+            </td>
+          </tr>
+        </template>
+      </Table>
+    </div>
+    <LazyModal v-if="modal.show">
+      <LazyFormMaterialEntregar
+        v-if="modal.action == 'mod'"
+        :id="reservaSeleccionada"
+        @actualizado="updateFiltrados"
+        @close="modal.show = !modal.show"
+      />
+      <LazyFormMaterialCreate
+        v-else-if="modal.action == 'add'"
+        @close="modal.show = !modal.show"
+      />
+      <LazyFormMaterialDelete
+        v-else-if="modal.action == 'del'"
+        @close="modal.show = !modal.show"
+      />
+    </LazyModal>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'AppLayout',
+  middleware: 'admin',
+  data() {
+    return {
+      pageHeader: {
+        reservas: [],
+        title: 'Reservas',
+        text: 'Reservas registradas en el sistema.',
+      },
+      table: {
+        header: ['Ci', 'Guardia', 'Deposito', 'inicio', 'fin', 'estado'],
+      },
+      modal: {
+        show: false,
+        action: '',
+      },
+      reservas: [],
+      contenidoFiltrado: [],
+      filtro: { contenido: '', id: 1 },
+      reservaSeleccionada: null,
+    };
+  },
+  computed: {
+    reservasAll() {
+      return this.$store.state.reservas.reservas;
+    },
+    filtrados() {
+      return this.$store.state.reservas.filtrados;
+    },
+    depositos() {
+      return this.$store.getters['depositos/conReservas'];
+    },
+    estados() {
+      return this.$store.getters['reservas/estadosConReserva'];
+    },
+  },
+  async mounted() {
+    await this.$store.dispatch('reservas/all');
+    this.reservas = this.reservasAll;
+    await this.$store.dispatch('depositos/all');
+    await this.$store.dispatch('categorias/all');
+  },
+  methods: {
+    seleccionarReserva(action, reserva = null) {
+      if (reserva) this.reservaSeleccionada = reserva.id;
+      if (action != 'view') {
+        this.modal.action = action;
+        this.modal.show = !this.modal.show;
+      }
+    },
+    verDeposito(dep) {
+      this.$store.dispatch('depositos/get', {
+        id: dep,
+      });
+    },
+    verCategoria(cat) {
+      this.$store.dispatch('categorias/get', {
+        id: cat,
+      });
+    },
+    filtrar() {
+      this.$store.dispatch('reservas/filtar', {
+        contenido: this.filtro.contenido,
+        id: this.filtro.id,
+      });
+      this.reservas = this.filtrados;
+    },
+    cambiarFiltro() {
+      if (this.filtro.contenido === 'deposito_id') {
+        this.contenidoFiltrado = this.depositos;
+      } else {
+        this.contenidoFiltrado = this.estados;
+      }
+      this.filtro.id = 0; //Limpia select
+      this.reservas = this.reservasAll;
+    },
+    updateFiltrados() {
+      this.modal.show = false;
+      this.reservas = this.reservasAll;
+    },
+    mostrarEstado(estado) {
+      switch (parseInt(estado)) {
+        case 1:
+          estado = 'Pendiente';
+          break;
+        case 2:
+          estado = 'Validada';
+          break;
+        case 3:
+          estado = 'Activa';
+          break;
+        case 4:
+          estado = 'Finalizada';
+          break;
+        default:
+          estado = 'Cancelada';
+      }
+      return estado;
+    },
+    claseEstado(estado) {
+      return `estado-${estado}`;
+    },
+  },
+};
+</script>
